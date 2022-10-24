f0bfe46ba31423d1f14891da61b36cad2bda601c
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-24 14:25:11 -0400

    change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Allen Li All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Allen Li
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
